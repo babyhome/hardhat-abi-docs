dbd168b06440035ac5615ace40c8597b64ed4a61
babyhome <dek.baan.baan@gmail.com>
2025-10-18 11:10:52 +0700

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default defineConfig(
       // Disable two rules that conflict with the patterns that we use
       "@typescript-eslint/require-await": "off",
       "@typescript-eslint/no-redundant-type-constituents": "off",
+      "@typescript-eslint/no-unused-vars": "error",
       // We disable the following rule as it's a common pattern when using
       // the network provider directly
       "@typescript-eslint/no-unsafe-assignment": "off",
